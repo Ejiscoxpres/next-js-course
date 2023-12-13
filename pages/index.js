70c465c45dfdeff4cb3346bc297267af63f7c38b
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-13 14:07:28 +0100

    adding getStaticProps to pages/server-side-rendering and Using filesystem

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function HomePage(props){
 }
 
 export async function getStaticProps(){
+    console.log('(Re-)Generating...');
     const filePath  = path.join(process.cwd(), 'data', 'dummy-backend.json')
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
@@ -21,5 +22,6 @@ export async function getStaticProps(){
     return {props:{
         products: data.products
     },
+    revalidate:10
 };
 } 
